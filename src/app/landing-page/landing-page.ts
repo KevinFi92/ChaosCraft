568233dc69b4b30d4d85be6228b5d077f40df17e
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2025-12-02 21:30:00 +0100

    Add landing page component and update app entry
    Introduces a new LandingPage component with HTML, SCSS, and test files. Updates app.html to use <app-landing-page> as the main entry, removes placeholder content, and applies new global styles and font. Adds assets for font and icon.

---
+++ src/app/landing-page/landing-page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.scss',
+})
+export class LandingPage {
+
+}
